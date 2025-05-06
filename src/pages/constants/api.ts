57fd778cb8a32f3205618ebceba97ecc79b108bd
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-05-06 16:08:48 -0300

    Update api.ts

--- src/pages/constants/api.ts
+++ src/pages/constants/api.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import { Axios } from 'axios'; // se você quiser criar instâncias específicas
 
-const api = axios.create({
-  baseURL: "https://api.disneyapi.dev",
+const api = Axios.create({
+  baseURL: 'https://api.disneyapi.dev/character',  
   timeout: 5000,
   headers: {
     "Content-Type": "application/json"
